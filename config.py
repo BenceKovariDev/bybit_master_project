5707fd3b7bf53f98bac3cb640b40df57a354e8c8
BenceKovariDev <Kovari19981202@gmail.com>
2026-06-27 09:33:49 +0000

    Fejezet 1.4: Biztonságos API kulcskezelés integrálása .env környezeti változókkal

--- config.py
+++ config.py
@@ -1,16 +1,17 @@
 # bybit_master_project/config.py
+import os
+from dotenv import load_dotenv
+
+# Betöltjük a .env fájl tartalmát a környezeti változók közé
+load_dotenv()
 
 class TradingConfig:
-    """A kereskedő robot globális beállításai"""
+    # Biztonságos kulcskezelés: ha nincs a .env-ben, egy üres stringet ad vissza
+    API_KEY = os.getenv("BYBIT_API_KEY", "")
+    API_SECRET = os.getenv("BYBIT_API_SECRET", "")
     
-    # API beállítások
-    API_URL = "https://api.bybit.com/v5/market/tickers"
-    CATEGORY = "spot"
-    
-    # Stratégia beállítások
-    MAX_POSITIONS = 3          # Egyszerre maximum ennyi coint tarthat a robot
-    STOP_LOSS_PCT = -1.0       # Ha a profit eléri a -1%-ot, azonnal eladunk (Védelmi vonal)
-    BUY_TRIGGER_PCT = 2.0      # Ha egy coin 2%-ot emelkedik 24h alatt, megvesszük
-    
-    # Rendszer beállítások
-    REFRESH_RATE_SECONDS = 5   # Hány másodpercenként frissítsen a monitor
+    # Kereskedési beállítások
+    MAX_POSITIONS = 3
+    BUY_TRIGGER_PCT = 2.0  # +2% emelkedésnél veszünk
+    STOP_LOSS_PCT = -1.0   # -1% esésnél eladunk
+    REFRESH_RATE_SECONDS = 5
